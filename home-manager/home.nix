22790a8139cf5b4bfc03d3856f49efdfd5a1aee0
Thomas Khalil <KernelPanicAUS@users.noreply.github.com>
2024-07-20 21:33:07 +0200

    add home-manager configs

---
+++ home-manager/home.nix
@@ -0,0 +1,105 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  imports = [
+    ./modules/git.nix
+    ./modules/zsh.nix
+    ./modules/tmux.nix
+    ./modules/alacritty.nix
+  ];
+  home.username = "tkhalil";
+  home.homeDirectory = "/Users/tkhalil";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # pkgs.hello
+    cachix
+    nil # Nix language server
+    nix-info
+    nixpkgs-fmt
+    nixci
+    direnv
+    git
+    gnupg
+    silver-searcher
+    skhd
+    vim
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  home.sessionVariables = {
+    TF_PLUGIN_CACHE_DIR = "$HOME/.terraform.d/plugin-cache";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Programs natively supported by home-manager.
+  programs = {
+    bat.enable = true;
+    bat.config.theme="OneHalfDark";
+    zoxide.enable = true;
+    fzf.enable = true;
+    jq.enable = true;
+    nix-index.enable = true;
+    htop.enable = true;
+    bash.enable = true;
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
+    gpg.enable = true;
+  };
+  services = {
+    gpg-agent = {
+      enable = pkgs.hostPlatform.isLinux;
+      enableExtraSocket = true;
+      enableSshSupport = true;
+      pinentryPackage = "mac";
+      defaultCacheTtl = 34560000;
+      maxCacheTtl = 34560000;
+    };
+  };
+}
---
+++ home-manager/modules/zsh.nix
@@ -0,0 +1,71 @@
+{
+  config,
+  pkgs,
+  ...
+}:{
+    programs.zsh = {
+      enable = true;
+      enableCompletion = true;
+      autosuggestion.enable = true;
+      oh-my-zsh.enable = true;
+      oh-my-zsh.plugins = ["git" "gcloud" "kubectl" "git-prompt"];
+      plugins = [
+        {
+          name = "zsh-autosuggestions";
+          src = pkgs.fetchFromGitHub {
+            owner = "zsh-users";
+            repo = "zsh-autosuggestions";
+            rev = "v0.7.0";
+            sha256 = "sha256-KLUYpUu4DHRumQZ3w59m9aTW6TBKMCXl2UcKi4uMd7w=";
+          };
+        }
+        {
+          name = "zsh-256color";
+          src = pkgs.fetchFromGitHub {
+            owner = "chrissicool";
+            repo = "zsh-256color";
+            rev = "9d8fa1015dfa895f2258c2efc668bc7012f06da6";
+            sha256 = "sha256-Qd9pjDSQk+kz++/UjGVbM4AhAklc1xSTimLQXxN57pI=";
+          };
+        }
+        {
+          name = "zsh-syntax-highlighting";
+          src = pkgs.fetchFromGitHub {
+            owner = "zsh-users";
+            repo = "zsh-syntax-highlighting";
+            rev = "0.7.0";
+            sha256 = "sha256-eRTk0o35QbPB9kOIV0iDwd0j5P/yewFFISVS/iEfP2g=";
+          };
+        }
+        {
+          name = "pi-theme";
+          file = "pi.zsh-theme";
+          src = pkgs.fetchFromGitHub {
+            owner = "tobyjamesthomas";
+            repo = "pi";
+            rev = "96778f903b79212ac87f706cfc345dd07ea8dc85";
+            sha256 = "0zjj1pihql5cydj1fiyjlm3163s9zdc63rzypkzmidv88c2kjr1z";
+          };
+        }
+      ];
+      initExtra = ''
+        export SSH_AUTH_SOCKET=$(gpgconf --list-dirs agent-ssh-socket)
+        export ZSH_THEME="pi"
+        autoload -U history-search-end #needed for -end
+        zle -N history-beginning-search-backward-end history-search-end
+        zle -N history-beginning-search-forward-end history-search-end
+
+        #when going up go up only beggining of curr word history
+        bindkey '\e[A' history-beginning-search-backward-end
+        bindkey '\e[B' history-beginning-search-forward-end
+        #ctrl + arrow forward/backward word
+        bindkey "^[[1;5C" forward-word
+        bindkey "^[[1;5D" backward-word
+        #alt + arrow forward/backward word
+        bindkey "^[[1;3C" forward-word
+        bindkey "^[[1;3D" backward-word
+        #alt + delete delete whole word
+        bindkey "\e\x7f" backward-kill-word
+      '';
+    };
+}
