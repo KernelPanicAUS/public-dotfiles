2f1b2194debcc329c923c3869a1cbd83daa4b8d3
Thomas Khalil <KernelPanicAUS@users.noreply.github.com>
2024-12-18 00:25:25 +0100

    add custom derivations for brew apps

--- hosts/uni.nix
+++ hosts/uni.nix
@@ -7,7 +7,48 @@
   ...
 }: let
   common = import ../packages/common.nix {inherit pkgs;};
-  additionalPackages = with pkgs; [lmstudio];
+  dervify = {
+    pname,
+    version,
+    url,
+    hash,
+  }:
+    pkgs.stdenv.mkDerivation (self: {
+      inherit pname;
+      inherit version;
+
+      src = pkgs.fetchurl {
+        inherit url;
+        inherit hash;
+        curlOptsList = ["-HUser-Agent: Wget/1.21.4" "-HAccept: */*"];
+      };
+
+      buildInputs = with pkgs; [undmg];
+
+      sourceRoot = ".";
+
+      installPhase = ''
+        runHook preInstall
+
+        mkdir -p "$out/Applications" "$out/bin"
+        cp -r *.app "$out/Applications"
+      '';
+    });
+
+  alfred = dervify rec {
+    pname = "Alfred";
+    version = "5.5";
+    url = "https://cachefly.alfredapp.com/${pname}_${version}.1_2273.dmg";
+    hash = "sha256-BopF9IV/JOpu/aViwV4nDxivlQUZmN+K3+f1/7BaN6M=";
+  };
+  lulu = dervify rec {
+    pname = "Lulu";
+    version = "2.9.6";
+    url = "https://github.com/objective-see/${pname}/releases/download/v${version}/${pname}_${version}.dmg";
+    hash = "sha256-OagnURumn+Aw5XBEbdz0LSEuhc3abY8h+RlXarzKgBk=";
+  };
+
+  additionalPackages = with pkgs; [lmstudio] ++ [lulu alfred];
 in {
   environment.systemPackages = common.commonPackages ++ additionalPackages;
   imports = [./common.nix];
@@ -21,9 +62,7 @@ in {
     brews = ["aria2"];
     casks = [
       "1password"
-      "alfred"
       "istat-menus"
-      "lulu"
       "orbstack"
       "hammerspoon"
     ];
