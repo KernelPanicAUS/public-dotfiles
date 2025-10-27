fd91aecc8ae1196d286e2f26f20277e7c188ad43
Thomas Khalil <KernelPanicAUS@users.noreply.github.com>
2025-10-27 12:18:07 +0100

    home-manager changed some inputs for the git module

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -5,24 +5,52 @@
 }: {
   programs.git = {
     enable = true;
-    userName = "Thomas Khalil";
-    userEmail = "KernelPanicAUS@users.noreply.github.com";
-    aliases = {
-      co = "checkout";
-      st = "status -s -b";
-      ci = "commit";
-      sb = "show-branch";
-      up = "!git fetch origin && git rebase origin/master";
-      lg = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %f %Cgreen(%cr)%Creset'";
-      who = "log --format=\"'%ai %h %aN %s'\"";
-      df = "difftool - -dir-diff";
-      ba = "branch -a";
-      br = "branch -r";
-      rpo = "remote prune origin";
-      undo = "reset --soft HEAD~1";
-      wip = "commit - am WIP";
-      spull = "!git pull && git submodule sync - -recursive && git submodule update - -init - -recursive";
-      spush = "push --recurse-submodules=on-demand";
+    settings = {
+      user = {
+        name = "Thomas Khalil";
+        email = "KernelPanicAUS@users.noreply.github.com";
+      };
+      alias = {
+        co = "checkout";
+        st = "status -s -b";
+        ci = "commit";
+        sb = "show-branch";
+        up = "!git fetch origin && git rebase origin/master";
+        lg = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %f %Cgreen(%cr)%Creset'";
+        who = "log --format=\"'%ai %h %aN %s'\"";
+        df = "difftool - -dir-diff";
+        ba = "branch -a";
+        br = "branch -r";
+        rpo = "remote prune origin";
+        undo = "reset --soft HEAD~1";
+        wip = "commit - am WIP";
+        spull = "!git pull && git submodule sync - -recursive && git submodule update - -init - -recursive";
+        spush = "push --recurse-submodules=on-demand";
+      };
+      core.pager = "${pkgs.delta}/bin/delta";
+      core.editor = "emacsclient";
+      delta = {
+        side-by-side = true;
+        navigate = true;
+        dark = true;
+      };
+      interactive.diffFilter = "${pkgs.delta}/bin/delta --color-only";
+      merge.conflictstyle = "zdiff3";
+      branch.autosetuprebase = "always";
+      push.autoSetupRemote = true;
+      log.decorate = true;
+      showbranch.default = "--all";
+      push.followTags = true;
+      diff.tool = "diffmerge";
+      diff.submodule = "log";
+      diff.wsErrorHighlight = "all";
+      core.excludesfile = "~/.gitignore";
+      #      core.editor = "/usr/bin/vim -f";
+      rebase.autoStash = true;
+      status.submoduleSummary = true;
+      init.defaultBranch = "main";
+      init.templateDir = "/Users/tkhalil/.git-template";
+      credential."https://github.com".helper = "!${pkgs.gh}/bin/gh auth git-credential";
     };
     ignores = [
       # System Files
@@ -70,32 +98,5 @@
       ".direnv/**"
       ".envrc"
     ];
-
-    extraConfig = {
-      core.pager = "${pkgs.delta}/bin/delta";
-      core.editor = "emacsclient -c -a ''";
-      delta = {
-        side-by-side = true;
-        navigate = true;
-        dark = true;
-      };
-      interactive.diffFilter = "${pkgs.delta}/bin/delta --color-only";
-      merge.conflictstyle = "zdiff3";
-      branch.autosetuprebase = "always";
-      push.autoSetupRemote = true;
-      log.decorate = true;
-      showbranch.default = "--all";
-      push.followTags = true;
-      diff.tool = "diffmerge";
-      diff.submodule = "log";
-      diff.wsErrorHighlight = "all";
-      core.excludesfile = "~/.gitignore";
-#      core.editor = "/usr/bin/vim -f";
-      rebase.autoStash = true;
-      status.submoduleSummary = true;
-      init.defaultBranch = "main";
-      init.templateDir = "/Users/tkhalil/.git-template";
-      credential."https://github.com".helper = "!${pkgs.gh}/bin/gh auth git-credential";
-    };
   };
 }
